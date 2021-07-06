6557fc264a1868d7d2d9c5bf12683b4e63a9d133
KankshitM <kankshitm2002@gmail.com>
2021-07-06 22:00:19 +0530

    First React APP

---
+++ DAY8/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from './COMPONENTS/card';
+import './App.css';
+import "./Style.css"
+
+function Cake(){
+	return(
+	<div className="Cake" > 
+		<Card />
+		
+	</div>
+	)
+}
+
+function Navbar(){
+	return <h1>NavigationBar</h1>;
+}
+
+export default Cake
---
+++ DAY8/src/COMPONENTS/card.jsx
@@ -0,0 +1,9 @@
+const Card =() =>{
+    return(<div className="card">
+        <img src="https://i.ytimg.com/vi/5L9dF0RYjNw/maxresdefault.jpg" alt="" />
+        <h2>NINJA</h2>
+        <p>KANKSH</p>
+    </div>)
+}
+
+export default Card
